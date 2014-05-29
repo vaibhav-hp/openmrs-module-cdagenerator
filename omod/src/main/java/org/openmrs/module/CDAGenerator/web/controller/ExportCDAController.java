d5de742b7bc9a417409fbeeb3f928de976e33a29
vaibhav-hp <kmit.vaibhav@gmail.com>
2014-05-30 01:47:16 +0530

    Commit with sample cda generation on click of export document button

--- omod/src/main/java/org/openmrs/module/CDAGenerator/web/controller/ExportCDAController.java
+++ omod/src/main/java/org/openmrs/module/CDAGenerator/web/controller/ExportCDAController.java
@@ -17,10 +17,32 @@ package org.openmrs.module.CDAGenerator.web.controller;
 
 
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openhealthtools.mdht.uml.cda.AssignedAuthor;
+import org.openhealthtools.mdht.uml.cda.Author;
+import org.openhealthtools.mdht.uml.cda.CDAFactory;
+import org.openhealthtools.mdht.uml.cda.ClinicalDocument;
+import org.openhealthtools.mdht.uml.cda.InfrastructureRootTypeId;
+import org.openhealthtools.mdht.uml.cda.Organization;
+import org.openhealthtools.mdht.uml.cda.Patient;
+import org.openhealthtools.mdht.uml.cda.PatientRole;
+import org.openhealthtools.mdht.uml.cda.Person;
+import org.openhealthtools.mdht.uml.cda.RecordTarget;
+import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
+import org.openhealthtools.mdht.uml.hl7.datatypes.CE;
+import org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory;
+import org.openhealthtools.mdht.uml.hl7.datatypes.II;
+import org.openhealthtools.mdht.uml.hl7.datatypes.PN;
+import org.openhealthtools.mdht.uml.hl7.datatypes.ST;
+import org.openhealthtools.mdht.uml.hl7.datatypes.TS;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.CDAGenerator.CDAHandlers.BaseCdaTypeHandler;
 import org.openmrs.module.CDAGenerator.api.CDAGeneratorService;
@@ -29,24 +51,115 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * The main controller.
  */
 
 @Controller
+@RequestMapping(value = "/module/CDAGenerator/exportcda*")
 public class ExportCDAController {
 	
 	protected final Log log = LogFactory.getLog(getClass());
 	
-	@ModelAttribute("ListCdatypes")
-	public List<BaseCdaTypeHandler> PopulateCdaTypes()
+	@RequestMapping(method = RequestMethod.POST)
+	public void manage(@RequestParam(value="patientId",required=true)org.openmrs.Patient p,HttpServletResponse response) {
+		ClinicalDocument doc = CDAFactory.eINSTANCE.createClinicalDocument();
+
+		InfrastructureRootTypeId typeId = CDAFactory.eINSTANCE.createInfrastructureRootTypeId();
+		typeId.setExtension("POCD_HD000040");
+		
+		doc.setTypeId(typeId);
+
+		II id = DatatypesFactory.eINSTANCE.createII("2.16.840.1.113883.19.4", "c266");
+		doc.setId(id);
+
+		II templateId = DatatypesFactory.eINSTANCE.createII("2.16.840.1.113883.3.27.1776");
+		doc.getTemplateIds().add(templateId);
+
+		CE code = DatatypesFactory.eINSTANCE.createCE("11488-4", "2.16.840.1.113883.6.1", "LOINC", "Consultation note");
+		doc.setCode(code);
+
+		ST title = DatatypesFactory.eINSTANCE.createST("");
+		doc.setTitle(title);
+
+		TS effectiveTime = DatatypesFactory.eINSTANCE.createTS("20000407");
+		doc.setEffectiveTime(effectiveTime);
+
+		CE confidentialityCode = DatatypesFactory.eINSTANCE.createCE("N", "2.16.840.1.113883.5.25");
+		doc.setConfidentialityCode(confidentialityCode);
+		RecordTarget recordTarget = CDAFactory.eINSTANCE.createRecordTarget();
+		doc.getRecordTargets().add(recordTarget);
+
+		PatientRole patientRole = CDAFactory.eINSTANCE.createPatientRole();
+		recordTarget.setPatientRole(patientRole);
+
+		org.openhealthtools.mdht.uml.cda.Patient patient = CDAFactory.eINSTANCE.createPatient();
+		patientRole.setPatient(patient);
+
+		PN name = DatatypesFactory.eINSTANCE.createPN();
+		name.addGiven("Henry").addFamily("Levin").addSuffix("the 7th");
+		patient.getNames().add(name);
+
+		CE administrativeGenderCode = DatatypesFactory.eINSTANCE.createCE("M", "2.16.840.1.113883.5.1");
+		patient.setAdministrativeGenderCode(administrativeGenderCode);
+
+		TS birthTime = DatatypesFactory.eINSTANCE.createTS("19320924");
+		patient.setBirthTime(birthTime);
+
+		Organization providerOrganization = CDAFactory.eINSTANCE.createOrganization();
+		providerOrganization.getIds().add(DatatypesFactory.eINSTANCE.createII("2.16.840.1.113883.19.5"));
+		patientRole.setProviderOrganization(providerOrganization);
+
+		Author author = CDAFactory.eINSTANCE.createAuthor();
+		author.setTime(DatatypesFactory.eINSTANCE.createTS("2000040714"));
+		doc.getAuthors().add(author);
+
+		AssignedAuthor assignedAuthor = CDAFactory.eINSTANCE.createAssignedAuthor();
+		assignedAuthor.getIds().add(DatatypesFactory.eINSTANCE.createII("2.16.840.1.113883.19.5", "KP00017"));
+		author.setAssignedAuthor(assignedAuthor);
+
+		Person assignedPerson = CDAFactory.eINSTANCE.createPerson();
+		assignedAuthor.setAssignedPerson(assignedPerson);
+
+		name = DatatypesFactory.eINSTANCE.createPN();
+		name.addGiven("Bob").addFamily("Dolin").addSuffix("MD");
+		assignedPerson.getNames().add(name);
+
+		 response.setHeader( "Content-Disposition", "attachment;filename=sampleTest.xml");	
+		   try {
+			  
+			 StringWriter r = new StringWriter();
+			 
+			  CDAUtil.save(doc, r);
+			  String ccdDoc = r.toString();
+			  ccdDoc = ccdDoc.replaceAll("&lt;", "<");
+			  ccdDoc = ccdDoc.replaceAll("&quot;", "\"");
+			  byte[] res = ccdDoc.getBytes(Charset.forName("UTF-8"));
+			  response.setCharacterEncoding("UTF-8");
+			  response.getOutputStream().write(res);
+			   response.flushBuffer();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			}
+	@RequestMapping( method = RequestMethod.GET)
+	public void PopulateCdaTypes(@RequestParam(value="patientId",required=false)Patient patient,ModelMap model) 
 	{
-		CDAGeneratorService cdaser=(CDAGeneratorService)Context.getService(CDAGeneratorService.class);
-		return cdaser.getAllCdaTypeChildHandlers();
-	}
-	@RequestMapping(value = "/module/CDAGenerator/exportcda", method = RequestMethod.GET)
-	public void manage(ModelMap model) {
+		
+		
+			CDAGeneratorService cdaser=(CDAGeneratorService)Context.getService(CDAGeneratorService.class);
+			List<BaseCdaTypeHandler> ListofCdatypes=cdaser.getAllCdaTypeChildHandlers();
+			model.addAttribute("ListCdatypes", ListofCdatypes);
 		
 	}
-}
+		
+		
+
+	}
+
